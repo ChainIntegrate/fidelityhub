a92f2fac6aefc2323eadd618c871d28631e3f8ff
Simone Cavalletti <info@chainintegrate.it>
2026-06-14 23:59:58 +0200

    chore: script deploy automatico VPS

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+﻿# Build frontend
+Set-Location client
+npm run build
+
+# Copia sulla VPS
+scp -r dist\* root@31.14.140.170:/var/www/fidelityhub/dist/
+
+# Esegui script VPS
+ssh root@31.14.140.170 "/root/deploy-fidelityhub.sh"
+
+Write-Host "Deploy completato!"
